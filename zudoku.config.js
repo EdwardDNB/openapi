87422959584dde87e345037b4e13911827e4d0fc
EdwardDNB <edvarddnbs@gmail.com>
2025-04-04 01:40:48 +0300

    readmy

---
+++ zudoku.config.js
@@ -0,0 +1,14 @@
+export default {
+	title: "Car Rental API",
+	spec: "./openApiFile.yaml",
+	nav: [
+		{ text: "Home", link: "/" },
+		{ text: "About", link: "/about" },
+	],
+	sidebar: [
+		{ text: "Car", link: "/Car" },
+		{ text: "DefaultApi", link: "/DefaultApi" },
+		{ text: "Reservation", link: "/Reservation" },
+		{ text: "About", link: "/about" },
+	],
+};
